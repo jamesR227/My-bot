fb9c4f8bce889e10e608711031e081f8ab835c4f
James Raymond <43977942+jamesR227@users.noreply.github.com>
2024-04-24 07:35:26 -0700

    Create moderation.js
    The moderation commands of the bot

---
+++ moderation.js
@@ -0,0 +1,11 @@
+module.exports = {
+    kickUser: function (message, args) {
+        // Check permissions, mention the user to kick, etc.
+        // Use message.guild.member(user).kick() to kick the user
+    },
+    banUser: function (message, args) {
+        // Check permissions, mention the user to ban, etc.
+        // Use message.guild.member(user).ban() to ban the user
+    }
+    // Add more moderation commands as needed
+};
